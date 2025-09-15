62d8ee0e44cdd057726f2d95ccbef3e14932c643
sosichh <mikhailgoncharskiy@yahoo.com>
2025-09-15 20:25:27 +0300

    add task 1 - domain

--- src/main/java/com/domain/Bike.java
+++ src/main/java/com/domain/Bike.java
@@ -1,5 +1,86 @@
 package com.domain;
 
+import java.util.Objects;
+
 public class Bike {
-    
+    private  int id;
+    private String title;
+    private String type;
+    private double price;
+    private boolean active;
+
+    public Bike() {
+    }
+
+    public Bike(String title, String type, double price) {
+        this.title = title;
+        this.type = type;
+        this.price = price;
+    }
+
+    public Bike(int id, String title, String type, double price, boolean active) {
+        this.id = id;
+        this.title = title;
+        this.type = type;
+        this.price = price;
+        this.active = active;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Bike bike = (Bike) o;
+        return id == bike.id && Double.compare(price, bike.price) == 0 && active == bike.active && Objects.equals(title, bike.title) && Objects.equals(type, bike.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, type, price, active);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Велосипед: id - %d, наименование - %s, тип - %s, цена - %.2f, активен - %b.",
+                id, title, type, price, active);
+    }
 }
--- src/main/java/com/domain/Customer.java
+++ src/main/java/com/domain/Customer.java
@@ -1,5 +1,74 @@
 package com.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Customer {
-    
+    private int id;
+    private String name;
+    private  boolean active;
+    private List<Bike> bikes = new ArrayList<>();
+
+    public Customer() {
+    }
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public List<Bike> getBikes() {
+        return bikes;
+    }
+
+    public void setBikes(List<Bike> bikes) {
+        this.bikes = bikes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && active == customer.active && Objects.equals(name, customer.name) && Objects.equals(bikes, customer.bikes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, active, bikes);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Покупатель: id - %d, имя - %s, активен - %b, лист покупок - %s.",
+                id, name, active, bikes);
+    }
 }
